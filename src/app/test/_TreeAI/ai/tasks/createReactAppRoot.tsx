ba9cd2178d387eefd79d6c23711a192b19a36fa6
Lok Lok <yellowhappy831@gmail.com>
2025-08-22 01:02:29 +0800

    save

--- src/app/test/_TreeAI/ai/tasks/createReactAppRoot.tsx
+++ src/app/test/_TreeAI/ai/tasks/createReactAppRoot.tsx
@@ -4,6 +4,7 @@ import {
     generateObject,
     generateText,
     ModelMessage,
+    streamObject,
     streamText,
     tool,
     UIMessage,
@@ -22,6 +23,7 @@ import { readFileContent } from "../../io/readFileContent";
 import { writeFileContent } from "../../io/writeFileContent";
 import { saveToBrowserDB } from "../../io/saveToBrowserDB";
 import z from "zod";
+import { XMLParser, XMLBuilder, XMLValidator } from "fast-xml-parser";
 
 export async function createReactAppRoot({
     userPrompt,
@@ -36,54 +38,68 @@ export async function createReactAppRoot({
     let info = [];
 
     let content = await readFileContent({ path: SPEC_DOC_PATH });
+    if (content) {
+        info.push({
+            role: "user",
+            content: `Here's the "product requirement definition": 
+${content}`,
+        });
+    }
 
     console.log("createReactAppRoot", content);
 
-    let response = await generateObject({
-        schema: z.array(
-            z.object({
-                filepath: z.string(),
-                filecontent: z.string(),
-            }),
-        ),
+    let response = streamText({
+        // schema: z
+        //     .array(
+        //         z
+        //             .object({
+        //                 path: z.string().describe("file path"),
+        //                 code: z.string().describe("code"),
+        //             })
+        //             .describe("file info"),
+        //     )
+        //     .describe("list of files and its content"),
+        // schemaName: "file outputs",
+        // schemaDescription: "a list of files to be written to file system",
         // toolChoice: "required",
         // tools: {
         //     ...IOTooling,
         // },
 
         messages: [
-            {
-                role: "system",
-                content: `
-You are a developer.
-
-You always use write file tool to write the genrated code into files accordinlg to each file.
-                `,
-            },
+            //             {
+            //                 role: "system",
+            //                 content: `
+            // You are a developer you are good at writing json.
+            //                 `,
+            //             },
             ...getModelMessagesFromUIMessages(),
             //
-            {
-                role: "user",
-                content: `Here's the "product requirement definition": 
-${content}`,
-            },
+            ...info,
 
             {
                 role: "user",
                 content: `
 Instructions:
 
-- only implement "/components/App.js" for the "product requirement definition":
+- Memorise the "product requirement definition", identify React Component modules and implement them in this format, use only javascript ".js" files:
+- DO NOT WRAP THE CODE WITH markdown
+- ONLY WRITE PURE CODE FOR {file_1_code} etc
+- the folder for components is at "/components/*"
+- use named export for "App" Component like the following: 
+export function App () {...}
 
-export function App () {
-    return <>
-        ...
-    </>
-}
+- include the following lines:
+import * as React from 'react';
 
-- Only output code, dont include markdown or text warpper
+- write the App component to "/components/App.js"
+- write the other components to "/components/*.js"
 
-- Write files to the right file
+
+- format please follow this:
+<x-mydearloklokcode file="{file_1_name}">
+{file_1_code}
+<x-mydearloklokcode>
                 `,
             },
             //
@@ -91,12 +107,34 @@ export function App () {
         model,
     });
 
-    console.log(response);
+    let text = "";
+    for await (let part of response.textStream) {
+        text += part;
+        console.log(text);
+    }
 
-    // await writeFileContent({
-    //     path: `${APP_ROOT_PATH}`,
-    //     content: response.text,
-    // });
+    let divDoc = document.createElement("div");
+    divDoc.innerHTML = text;
+
+    let allCodes = divDoc.querySelectorAll("x-mydearloklokcode");
+
+    for (let i = 0; i < allCodes.length; i++) {
+        let path = allCodes[i].attributes[0]?.value;
+        let content = allCodes[i].innerHTML;
+
+        if (path.startsWith("/")) {
+        } else {
+            path = `/${path}`;
+        }
+
+        await writeFileContent({
+            path: `${path}`,
+            content: content,
+        });
+    }
+
+    // console.log(await response.object);
+
     await saveToBrowserDB();
 
     MyTaskManager.doneTask("createReactAppRoot");
