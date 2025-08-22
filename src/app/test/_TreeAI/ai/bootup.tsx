183cfba569c67cd9cf533a37183e4fa3582b6131
Lok Lok <yellowhappy831@gmail.com>
2025-08-22 12:55:03 +0800

    boot

--- src/app/test/_TreeAI/ai/bootup.tsx
+++ src/app/test/_TreeAI/ai/bootup.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { loadFromBrowserDB } from "../io/loadFromBrowserDB";
-import { useAI } from "../state/useAI";
+import { EngineSetting, useAI } from "../state/useAI";
 import { createInstance } from "localforage";
 // import { bootEngines } from "./bootEngines";
 
@@ -9,38 +9,6 @@ export const bootup = async () => {
 };
 
 export const SettingsBootUp = () => {
-    useEffect(() => {
-        useAI.getState().engines[0].enabled = true;
-        useAI.getState().engines[1].enabled =
-            localStorage.getItem("engine1") === "ok";
-        useAI.getState().engines[2].enabled =
-            localStorage.getItem("engine2") === "ok";
-        useAI.getState().engines[3].enabled =
-            localStorage.getItem("engine3") === "ok";
-        useAI.getState().engines[4].enabled =
-            localStorage.getItem("engine4") === "ok";
-        useAI.getState().engines[5].enabled =
-            localStorage.getItem("engine5") === "ok";
-        useAI.getState().engines[6].enabled =
-            localStorage.getItem("engine6") === "ok";
-
-        useAI.setState({
-            engines: [...useAI.getState().engines],
-        });
-
-        return useAI.subscribe((now, before) => {
-            if (now.engines !== before.engines) {
-                now.engines.forEach((engine, idx) => {
-                    if (engine.enabled) {
-                        localStorage.setItem(`engine${idx}`, "ok");
-                    } else {
-                        localStorage.setItem(`engine${idx}`, "bad");
-                    }
-                });
-            }
-        });
-    }, []);
-
     let appID = useAI((r) => r.appID);
 
     useEffect(() => {
@@ -75,10 +43,14 @@ export const SettingsBootUp = () => {
             name: `engines-${appID}`,
         });
 
-        db.getItem("engine-settings").then((msg) => {
-            if (msg instanceof Array) {
+        db.getItem("engine-settings").then((engSettting: EngineSetting[]) => {
+            if (engSettting instanceof Array) {
+                engSettting = engSettting.map((it) => {
+                    it.status = "empty";
+                    return it;
+                });
                 useAI.setState({
-                    engines: msg,
+                    engines: engSettting,
                 });
             }
         });
@@ -88,6 +60,38 @@ export const SettingsBootUp = () => {
             }
         });
     }, [appID]);
+
+    useEffect(() => {
+        useAI.getState().engines[0].enabled = true;
+        useAI.getState().engines[1].enabled =
+            localStorage.getItem("engine1") === "ok";
+        useAI.getState().engines[2].enabled =
+            localStorage.getItem("engine2") === "ok";
+        useAI.getState().engines[3].enabled =
+            localStorage.getItem("engine3") === "ok";
+        useAI.getState().engines[4].enabled =
+            localStorage.getItem("engine4") === "ok";
+        useAI.getState().engines[5].enabled =
+            localStorage.getItem("engine5") === "ok";
+        useAI.getState().engines[6].enabled =
+            localStorage.getItem("engine6") === "ok";
+
+        useAI.setState({
+            engines: [...useAI.getState().engines],
+        });
+
+        return useAI.subscribe((now, before) => {
+            if (now.engines !== before.engines) {
+                now.engines.forEach((engine, idx) => {
+                    if (engine.enabled) {
+                        localStorage.setItem(`engine${idx}`, "ok");
+                    } else {
+                        localStorage.setItem(`engine${idx}`, "bad");
+                    }
+                });
+            }
+        });
+    }, []);
 
     return <></>;
 };
