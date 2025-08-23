70a4083ddd42bc93342d4da966336af12a69cfba
Lok Lok <yellowhappy831@gmail.com>
2025-08-23 17:10:20 +0800

    save

--- src/components/_TreeAI/ai/getModelMessagesFromUIMessages.tsx
+++ src/components/_TreeAI/ai/getModelMessagesFromUIMessages.tsx
@@ -4,5 +4,12 @@ import { useAI } from "../state/useAI";
 export const getModelMessagesFromUIMessages = () => {
     let uiMessages = useAI.getState().uiMessages;
 
+    uiMessages.filter((r) => {
+        if (r.parts.some((r) => r.type.startsWith("data-"))) {
+            return false;
+        }
+        return true;
+    });
+
     return convertToModelMessages(uiMessages);
 };
