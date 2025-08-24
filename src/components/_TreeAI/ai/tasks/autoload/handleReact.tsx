c7b592f7f7508da2966c79d24a4a752a46fbe4d6
Lok Lok <yellowhappy831@gmail.com>
2025-08-24 12:00:21 +0800

    save

--- src/components/_TreeAI/ai/tasks/autoload/handleReact.tsx
+++ src/components/_TreeAI/ai/tasks/autoload/handleReact.tsx
@@ -86,34 +86,6 @@ import * as React from 'react';
 - use some "p-3" padding spacing 
 - uses Zustand state management files located at "/store/*.ts" folder
 
-------
-
-- save JWT to zustand and localStorage with appID as Key after user loggin successfully
-- save JWT to zustand and localStorage with appID as Key after user registered successfully
-
-- Responsive UI component
-- UI component shouldn't have any margin pixel for the outermost div element.
-- use Zustand for State Management for React
-- Loading and error states
-- Have import and export using ESM for React Component Code
-- Include login, register, dashboard
-
-- Cannot use zustand selector as inline varaible
-- build a simple layout
-- single page application with <HashRouter> from react-router-dom
-- const location = useLocation()  
-- use location.pathname instead of location.hash
-
-## Overall 
-
-- Tailwind CSS for styling for Frontend CSS Styling
-- Javascript preferred over typescript
-- React + Javascript for Frontend HTML
-- @react-three/fiber + drei for Frontend 3D
-- Zustand for state management for Frontend State Management
-
-
-
 ${await getFileOutputFormatting()}
 
                 `,
