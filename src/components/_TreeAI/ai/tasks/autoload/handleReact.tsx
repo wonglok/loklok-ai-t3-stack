076e0cb852be90f07df6eee9b03061b9e8bbdc9d
Lok Lok <yellowhappy831@gmail.com>
2025-08-26 10:20:31 +0800

    handled

--- src/components/_TreeAI/ai/tasks/autoload/handleReact.tsx
+++ src/components/_TreeAI/ai/tasks/autoload/handleReact.tsx
@@ -76,12 +76,12 @@ export async function handleReact({
 # Instruction:
 
 - Identify ALL React Component including user login register, landing page, and implement them in javascript format, use only javascript ".jsx" files:
+- MUST write to the folder for components is at "/components/*"
 
 # Requirements:
 
 - MUST NOT WRAP THE CODE WITH markdown
 - ONLY WRITE PURE CODE FOR {code} etc
-- the folder for components is at "/components/*"
 - the folder for util is at "/util/*"
 
 - MUST NOT USE '@/...' to import modules
@@ -113,6 +113,8 @@ import * as React from 'react';
 
 - MUST use hash link: <a href="#/register">Register</a>
 - MUST NOT use url link: <a href="/register">Register</a>
+- MUST NOT use a link within a link: <a href="/register">Register  <a href="/register">Register</a> </a> // bad practice
+
 
 - MUST not: In HTML, <a> cannot be a descendant of <a>. This will cause a hydration error.
 
